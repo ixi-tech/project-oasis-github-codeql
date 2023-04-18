6027e4b1c4f1ce0aca31bed689ee40f69caba6c4
Marco Solina <marco.solina@ixico.com>
2023-04-18 16:22:21 +0200

    test

---
+++ SimpleProject/TestCodeQl.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleProject
+{
+    //https://codeql.github.com/codeql-query-help/csharp/cs-invalid-dynamic-call/
+    public class TestCodeQl
+    {
+        class WithFoo
+        {
+            public void Foo(int i) { }
+        }
+
+        class WithoutFoo { }
+
+        public void DoSomething()
+        {
+            dynamic o = new WithoutFoo();
+            o.Foo(3);
+        }
+    }
+}
