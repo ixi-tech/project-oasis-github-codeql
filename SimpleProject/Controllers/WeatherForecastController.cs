ebe5a4bd25b49f3dab9c45448749dc22a0595e7b
Marco Solina <marco.solina@ixico.com>
2023-04-19 09:25:56 +0200

    test

--- SimpleProject/Controllers/WeatherForecastController.cs
+++ SimpleProject/Controllers/WeatherForecastController.cs
@@ -8,8 +8,10 @@ namespace SimpleProject.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly ILogger<WeatherForecastController> _logger;
+        private static string host = "https://localhost:8081";
+        private static string password = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-        private IWeatherData repo = new LocalCosmosEmulator("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+        private IWeatherData repo = new LocalCosmosEmulator(host, password);
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
